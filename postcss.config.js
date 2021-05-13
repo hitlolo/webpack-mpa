0940c1e711695501ef3eec5482784277edd88e84
hitlolo <hitlolo@qq.com>
2021-05-13 19:21:32 +0800

    update postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -4,12 +4,6 @@ module.exports = {
     // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
     plugins: [
         require('autoprefixer'),
-        // https://github.com/maximkoretskiy/postcss-autoreset
-        require('postcss-autoreset')(),
-        // https://github.com/maximkoretskiy/postcss-initial
-        require('postcss-initial')({
-            reset: 'inherited' // reset only inherited rules
-        }),
         // https://purgecss.com/plugins/postcss.html#installation
         require('@fullhuman/postcss-purgecss')({
             content: ['./src/**/*.html']
